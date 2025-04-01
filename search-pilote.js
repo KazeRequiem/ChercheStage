4c76c1b9e9f4c4632ed64829d57f7442da364fa3
Lukas PRETRE <lukas.pretre@viacesi.fr>
2025-04-01 23:50:54 +0200

    barre de recherche fonctionnel

---
+++ search-pilote.js
@@ -0,0 +1,43 @@
+const searchInput = document.querySelector("#search");
+const searchResults = document.querySelector(".candidatures-publiées");
+
+function createPiloteList(data) {
+
+    if (data.length === 0) {
+        // Si aucun pilote n'est trouvé, afficher un message
+        const noResultsMessage = document.createElement("p");
+        noResultsMessage.textContent = "Aucun pilote trouvé.";
+        searchResults.appendChild(noResultsMessage);
+        return;
+    }
+
+    data.forEach(pilote => {
+        const listItem = document.createElement("div");
+        listItem.setAttribute("class", "candidature-item");
+
+        listItem.innerHTML = `
+            <div>
+                <h4>${pilote.title}</h4>
+                <p>🧑‍🎓 ${pilote.firstName}</p>
+                <p>📄 ${pilote.promotion}</p>
+                <p>📍 ${pilote.location}</p>
+            </div>
+            <div>
+                <a href="${pilote.editLink}" class="modifier">Modifier</a>
+            </div>
+        `;
+
+        searchResults.appendChild(listItem);
+    });
+}
+
+searchInput.addEventListener("input", function () {
+    const searchValue = this.value.toLowerCase();
+    const filteredPilotes = pilotes.filter(pilote =>
+        pilote.firstName.toLowerCase().includes(searchValue) ||
+        pilote.promotion.toLowerCase().includes(searchValue)
+    );
+    createPiloteList(filteredPilotes);
+});
+
+createPiloteList(pilotes);
