1a99d74cb7953cc3e677b08cc6d9bc3f99ff077e
Lukas Prêtre <lukas.pretre@viacesi.fr>
2025-04-01 09:43:42 +0200

    Ajout des pages entreprises

---
+++ chart-entreprise.js
@@ -0,0 +1,60 @@
+const chart = document.getElementById('barCanvas');
+
+const data = {
+    labels: ['Réponses envoyées', 'Candidatures consulté', 'Candidatures reçus'],
+    datasets: [{
+        label: 'Candidatures',
+        data: [30, 50, 70],
+        backgroundColor: [
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(255, 159, 64, 0.7)',
+            'rgba(54, 162, 235, 0.7)'
+        ],
+        hoverBackgroundColor: [
+            'rgba(75, 192, 192, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(54, 162, 235, 1)'
+        ],
+        borderWidth: 2
+    }]
+};
+
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'bottom',
+            labels: {
+                font: {
+                    size: 14
+                }
+            }
+        },
+        tooltip: {
+            enabled: true
+        }
+    },
+    cutout: '70%'
+};
+
+new Chart(chart, {
+    type: 'doughnut',
+    data: data,
+    options: options
+});
+
+document.querySelectorAll('.rating span').forEach(star => {
+    star.addEventListener('click', function () {
+        const rating = this.getAttribute('data-value');
+        document.getElementById('rating-value').textContent = `Votre note : ${rating}/5`;
+
+        document.querySelectorAll('.rating span').forEach(s => s.classList.remove('selected'));
+
+        this.classList.add('selected');
+        let nextSibling = this;
+        while (nextSibling) {
+            nextSibling.classList.add('selected');
+            nextSibling = nextSibling.nextElementSibling;
+        }
+    });
+});
